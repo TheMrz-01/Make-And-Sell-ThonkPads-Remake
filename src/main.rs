f22e2c68fa939c7e77056370fddf87a184683486
Mrz <86775732+TheMrz-01@users.noreply.github.com>
2024-06-11 12:59:14 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,11 @@ struct Name(String);
 
 fn main() {
    App::new()
-       .add_systems(Startup, add_people)
-       .add_systems(Update, (hello_world, greet_people))
-       .run();
+      .insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+      .add_plugins(DefaultPlugins)
+      .add_systems(Startup, add_people)
+      .add_systems(Update, (update_people, greet_people.chain()))
+      .run();
 }
 
 fn hello_world() {
@@ -23,8 +25,22 @@ fn add_people(mut commands: Commands) {
    commands.spawn((Person, Name("Zayna Nieves".to_string())));
 }
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-   for name in &query {
-       println!("hello {}!", name.0);
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
+fn greet_people(time: Res<Time>,mut timer: ResMut<GreetTimer>,query: Query<&Name, With<Person>>) {
+   if timer.0.tick(time.delta()).just_finished() {
+      for name in &query {
+          println!("hello {}!", name.0);
+      }
+  }
+}
+
+fn update_people(mut query: Query<&mut Name, With<Person>>) {
+   for mut name in &mut query {
+       if name.0 == "Elaina Proctor" {
+           name.0 = "Elaina Hume".to_string();
+           break; // We don’t need to change any other names
+       }
    }
 }
