30b604a871fddcdc055fae62292708d1cdf93e9b
Mrz <86775732+TheMrz-01@users.noreply.github.com>
2024-06-16 22:44:11 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -12,58 +12,25 @@ fn main() {
 }
 
 #[derive(Component)]
-struct player;
-
-#[derive(Component)]
-struct position {x: f32,y: f32,z: f32}
-
-#[derive(Component)]
 struct speed(u8);
 
 fn setup(mut commands: Commands,
-   mut meshes: ResMut<Assets<Mesh>>,
-   mut materials: ResMut<Assets<ColorMaterial>>,)
+    asset_server: Res<AssetServer>)
    {
-    commands.spawn((player,speed(8)));
-    commands.spawn((player,position{x: 0.0,y: 0.0,z: 0.0}));
+    commands.spawn((
+        SpriteBundle {
+            texture: asset_server.load("Gokhan Hitler.png"),
+            transform: Transform::from_xyz(100., 0., 0.),
+            ..default()
+        },
+        speed(8),
+    ));
 
     commands.spawn(Camera2dBundle::default());   
-
-    let shape: Mesh2dHandle = Mesh2dHandle(meshes.add(Triangle2d::new(
-        Vec2::Y * 50.0,
-        Vec2::new(-50.0, -50.0),
-        Vec2::new(50.0, -50.0),
-    )));
-
-    let color = Color::rgb_u8(255, 17, 0);
-
-    commands.spawn(MaterialMesh2dBundle {
-        mesh: shape,
-        material: materials.add(color),
-        transform: Transform::from_xyz(
-            0.0,
-            0.0,
-            0.0,
-        ),
-        ..default()
-    });
-
 }
 
 fn movement(keyboard_input: Res<ButtonInput<KeyCode>>,
-    p_position: Query<&mut position, With<player>>,
-    p_speed: Query<&speed, With<player>>
+    time: Res<Time>,mut player: Query<(&mut speed, &mut Transform)>
 ){
-    if keyboard_input.pressed(KeyCode::KeyW){
-        p_position = 150. * time.delta_seconds()
-    }
-    if keyboard_input.pressed(KeyCode::KeyS){
-
-    }
-    if keyboard_input.pressed(KeyCode::KeyD){
-
-    }
-    if keyboard_input.pressed(KeyCode::KeyA){
-
-    }
+    
 }
