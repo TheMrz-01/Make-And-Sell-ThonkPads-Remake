60bd04d73b9ac8429ebe4da9b08a46feace95ffa
Mrz <86775732+TheMrz-01@users.noreply.github.com>
2024-06-17 14:52:31 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-use bevy::{
-   prelude::*,
-   sprite::{MaterialMesh2dBundle, Mesh2dHandle},
-};
+use std::u8;
+
+use bevy::{math::f32, prelude::*};
 
 fn main() {
    App::new()
@@ -12,25 +11,45 @@ fn main() {
 }
 
 #[derive(Component)]
-struct speed(u8);
+struct Speed(f32);
+#[derive(Component)]
+struct sprintSpeed(f32);
 
 fn setup(mut commands: Commands,
     asset_server: Res<AssetServer>)
    {
+    commands.spawn(Camera2dBundle::default());  
+
     commands.spawn((
         SpriteBundle {
-            texture: asset_server.load("Gokhan Hitler.png"),
-            transform: Transform::from_xyz(100., 0., 0.),
+            texture: asset_server.load("GokhanHitler.png"),
+            transform: Transform::from_xyz(0., 0., 0.),
             ..default()
         },
-        speed(8),
+        Speed(100.),
+        sprintSpeed(200.),
     ));
 
-    commands.spawn(Camera2dBundle::default());   
+
 }
 
 fn movement(keyboard_input: Res<ButtonInput<KeyCode>>,
-    time: Res<Time>,mut player: Query<(&mut speed, &mut Transform)>
+    time: Res<Time>,mut player: Query<(&mut sprintSpeed,&mut Speed, &mut Transform)>
 ){
-    
+    for (mut sprintspeed,mut speed,mut transform) in &mut player{
+        if keyboard_input.pressed(KeyCode::ShiftLeft) {speed.0 = sprintspeed.0;}
+        
+        if keyboard_input.pressed(KeyCode::KeyW) {
+            transform.translation.y +=  speed.0 * time.delta_seconds()
+        }
+        if keyboard_input.pressed(KeyCode::KeyS) {
+            transform.translation.y -=  speed.0 * time.delta_seconds()
+        }
+        if keyboard_input.pressed(KeyCode::KeyD) {
+            transform.translation.x +=  speed.0 * time.delta_seconds()
+        }
+        if keyboard_input.pressed(KeyCode::KeyA) {
+            transform.translation.x -=  speed.0 * time.delta_seconds()
+        }
+    }
 }
