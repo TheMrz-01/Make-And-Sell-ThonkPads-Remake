56892fc3c2ebb5a85c7a42e3e1117422049dcf81
Mrz <86775732+TheMrz-01@users.noreply.github.com>
2024-06-18 14:18:27 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,69 @@
-use std::u8;
+use std::f32::consts::E;
 
-use bevy::{math::f32, prelude::*};
+use bevy::{ecs::component, prelude::*};
 
 fn main() {
    App::new()
        .add_plugins(DefaultPlugins)
        .add_systems(Startup, setup)
-       .add_systems(Update, movement)
+       .add_systems(Update, move_player)
        .run();
 }
 
 #[derive(Component)]
-struct Speed(f32);
-#[derive(Component)]
-struct sprintSpeed(f32);
+struct Speed{
+    normalSpeed: f32,
+    sprintSpeed: f32,
+}
+
+
+#[derive(Bundle)]
+struct PlayerBundle{
+    speed: Speed,
+    p_sprite: SpriteBundle,
+}
+
 
 fn setup(mut commands: Commands,
     asset_server: Res<AssetServer>)
    {
     commands.spawn(Camera2dBundle::default());  
 
-    commands.spawn((
-        SpriteBundle {
-            texture: asset_server.load("GokhanHitler.png"),
-            transform: Transform::from_xyz(0., 0., 0.),
-            ..default()
-        },
-        Speed(100.),
-        sprintSpeed(200.),
-    ));
+    commands.spawn(
+        PlayerBundle{
+            speed: Speed{
+                normalSpeed: 150.,
+                sprintSpeed: 200.,
+            },
+            p_sprite: SpriteBundle {
+                texture: asset_server.load("GokhanHitler.png"),
+                transform: Transform::from_xyz(0., 0., 0.),
+                ..default()                
+            }
+        }
+    );
 
 
 }
 
-fn movement(keyboard_input: Res<ButtonInput<KeyCode>>,
-    time: Res<Time>,mut player: Query<(&mut sprintSpeed,&mut Speed, &mut Transform)>
+fn move_player(keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut query: Query<(&Speed, &mut Transform)>, time: Res<Time>,
 ){
-    for (mut sprintspeed,mut speed,mut transform) in &mut player{
-        if keyboard_input.pressed(KeyCode::ShiftLeft) {speed.0 = sprintspeed.0;}
+    for (speed, mut transform) in query.iter_mut(){
+        let mut movement_speed = speed.normalSpeed;
+        if keyboard_input.pressed(KeyCode::ShiftLeft) {movement_speed = speed.sprintSpeed;}
         
         if keyboard_input.pressed(KeyCode::KeyW) {
-            transform.translation.y +=  speed.0 * time.delta_seconds()
+            transform.translation.y +=  movement_speed * time.delta_seconds()
         }
         if keyboard_input.pressed(KeyCode::KeyS) {
-            transform.translation.y -=  speed.0 * time.delta_seconds()
+            transform.translation.y -=  movement_speed * time.delta_seconds()
         }
         if keyboard_input.pressed(KeyCode::KeyD) {
-            transform.translation.x +=  speed.0 * time.delta_seconds()
+            transform.translation.x +=  movement_speed * time.delta_seconds()
         }
         if keyboard_input.pressed(KeyCode::KeyA) {
-            transform.translation.x -=  speed.0 * time.delta_seconds()
+            transform.translation.x -=  movement_speed * time.delta_seconds()
         }
     }
 }
